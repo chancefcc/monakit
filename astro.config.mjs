349f8b769d3ee90dc151baeb1559969ec4bac47f
Chance J. <chancefcc@gmail.com>
2026-01-14 01:10:19 +0800

    Phase 2: Install new dependencies (SolidJS, Astro 4, TailwindCSS 3)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
-import solidJs from "@astrojs/solid-js"
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,4 +14,4 @@ export default defineConfig({
     solidJs(),
     tailwind({ applyBaseStyles: false }),
   ],
-})
+});
